b4ac075c5dba60ec0562235bf1a411e768e9d522
mikaelalmondes01 <mikaelalmondes10@gmail.com>
2021-08-17 19:53:59 -0300

    alteracao

--- client/pages/sobre.vue
+++ client/pages/sobre.vue
@@ -2,17 +2,38 @@
   <div>
     <p class="text-top">OBJETIVO DA APLICAÇÃO</p>
     <div class="text-top">
-      Tema utilizado foi a construção civil, o qual tal aplicação foi criada com
-      base no site https://maraba.pa.gov.br/ em que fizemos uma pesquisa sobre
-      os projetos de construção pela aba de transparencia do site onde podemos
-      notar a discrepancia entre os valores estimados para a obra e o custo
-      final do projeto.
+      O objetivo desta aplicação é fazer a analise de dados de projetos de contrução civil
+      da cidade de Marabá-PA. O processo de licitação de um projeto na cidade é documentado e expresso 
+      no site do municipio, presente em https://maraba.pa.gov.br/ , todo projeto detêm alguns parametros
+      como o código, data de inicio e fim do projeto, o valor previsto de gastos iniciais e o valor liquidado,
+      valor esse representando o total de gastos que o projeto deteve, desse modo é o valor final pago pela 
+      prefeitura.
+      Nessa perspectiva existe um problema, pois grande parte das vezes os projetos detêm um valor liquidado final com até
+      600% do valor inicial de acrescimo, desse modo demonstra-se um superfaturamento exarcebado nos projetos,
+      que pode ser por diversos fatores, entretanto para uma boa gestão é necessário mapear essa problemática e 
+      com esse intuito nossa aplicação resume em um gráfico essa analise de discrepância entre os valores previstos
+      e o real valor gasto pela prefeitura desde 2019 a a 2022(valores pagos até o momento).  
+     
     </div>
-    <div class="text-top">
-      Para tal, estamos utilizando uma tabela para a verificação dos valores e
-      um gráfico para acompanhar a diferença no período de 2018 até agosto de
-      2020
-    </div>
+   <b-card-group deck>
+    <b-card title="Title" img-src="https://instagram.fmab1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/82878889_555066915090242_7732950829602177024_n.jpg?_nc_ht=instagram.fmab1-1.fna.fbcdn.net&_nc_ohc=HUaDtMTxfvwAX-LJUqL&edm=AP_V10EBAAAA&ccb=7-4&oh=7b9336861fe7f94dc0d9cd056ad589d7&oe=61237B97&_nc_sid=4f375e" img-alt="Image" img-top>
+      <b-card-text>
+        Desenvolvedor: Mikael Almondes da Silva
+        Curso: Engenharia da Computação
+        Turma: 2017
+        Disciplina: Banco de Dados e Desenvolvimento WEB
+      </b-card-text>
+    </b-card>
+
+    <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+      <b-card-text>
+        Desenvolvedor: Kristhyan de Matos Maia
+        Curso: Engenharia da Computação
+        Turma: 2017
+        Disciplina: Banco de Dados e Desenvolvimento WEB
+      </b-card-text>
+    </b-card>
+ 
   </div>
 </template>
 
